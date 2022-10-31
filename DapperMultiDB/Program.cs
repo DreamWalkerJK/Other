fc67336e8bb1f05a72f781c578d8a729e6aeaed6
DreamWalkerJK <lunatic_jk@qq.com>
2022-10-31 17:12:50 +0800

    增、改、查、删测试

--- DapperMultiDB/Program.cs
+++ DapperMultiDB/Program.cs
@@ -7,15 +7,45 @@ namespace DapperMultiDB
     {
         static void Main(string[] args)
         {
-            var classList = MySQLOperate.Query<Class>(string.Format("select * from Class where 1 = 1"));
-            //var classList = PostgreSQLOperate.Query<Class>(string.Format("select * from public.Class where 1 = 1"));
-            //var classList = SQLiteOperate.Query<Class>(string.Format("select * from Class where 1 = 1"));
-            //var classList = SQLiteOperate.Query<Class>(string.Format("select * from Class where 1 = 1"));
+            var addClass = new Class
+            {
+                CId = "C_1005",
+                CName = "Temp"
+            };
 
-            foreach(var c in classList)
+            // 增
+            //var addTest = string.Format(@"insert into Class(CId, CName) values(@CId, @CName)");
+            //PostgreSQLOperate.Add(addTest, addClass);
+
+            //改
+            //addClass.CName = "Temp1";
+            //var updateTest = string.Format(@"Update public.Class set CName=@CName where CId=@CId");
+            //PostgreSQLOperate.Update(updateTest, addClass);
+
+            //查
+            //var classList = MySQLOperate.Query<Class>(string.Format(@"select * from Class where 1 = 1"));
+            var classList = PostgreSQLOperate.Query<Class>(string.Format(@"select * from public.Class where 1 = 1"));
+            //var classList = SQLiteOperate.Query<Class>(string.Format(@"select * from Class where 1 = 1"));
+            //var classList = SQLiteOperate.Query<Class>(string.Format(@"select * from Class where 1 = 1"));
+
+            //批量查询
+            //var ids = new string[2] { "c_1002", "c_1003" };
+            //var queryTest = "select * from public.Class where CId = ANY (@ids)";
+            //var classList = PostgreSQLOperate.Query<Class>(queryTest, ids);
+
+            foreach (var c in classList)
             {
                 Console.WriteLine($"{c.CId} : {c.CName}");
             }
+
+            // 删
+            //PostgreSQLOperate.Delete(string.Format(@"delete from public.Class where CId=@CId;"), addClass);
+            //classList = PostgreSQLOperate.Query<Class>(string.Format(@"select * from public.Class where 1 = 1"));
+
+            //foreach (var c in classList)
+            //{
+            //    Console.WriteLine($"{c.CId} : {c.CName}");
+            //}
 
             Console.ReadKey();
         }
